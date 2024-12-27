0f7fa4158f39019395bdc1737789de49b73c46f5
kavyaramesh18 <122973945+kavyaramesh18@users.noreply.github.com>
2024-12-27 20:58:19 +0530

    Create Java.java

---
+++ Java.java
@@ -0,0 +1,13 @@
+package com.example.maven_project;
+
+public class Java {
+    public static void main(String[] args) {
+        System.out.println("Welcome to your Maven Project!");
+        System.out.println("2 + 3 = " + add(2, 3));
+    }
+
+    // A simple method to add two numbers
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
